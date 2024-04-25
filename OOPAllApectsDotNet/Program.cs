e542eb99fa3dfdc6562d09fac35ff6e9d79fb10e
fbozas <fbozas@hotmail.com>
2024-04-25 19:26:41 +0300

    Added Inheritance Grouping

--- OOPAllApectsDotNet/Program.cs
+++ OOPAllApectsDotNet/Program.cs
@@ -20,7 +20,23 @@ namespace OOPAllApectsDotNet
 			//Console.WriteLine(person.ToString());
 
 			Student student = new Student("Nick Lamprou");
-            Console.WriteLine(student.Name);
+			Student student1 = new Student("Peter Komninos");
+			Student student2 = new Student("Kostas Pappas");
+			Student student3 = new Student("Panos Bekas");
+			Student student4 = new Student("John Pappas");
+			Student student5 = new Student("Tasos Lelakis");
+			Student student6 = new Student("Kostas Lamprou");
+
+			Console.WriteLine(student.Name);
+
+            Console.WriteLine("-----------");
+
+			// Inheritance grouping
+			List<Person> inheritanceList = new List<Person>()
+			{ 
+				student,student1,student2, student3, student4,
+				student5,student6
+			};
         }
 	}
 }
